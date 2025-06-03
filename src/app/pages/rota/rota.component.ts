d16ced29728e723429a8933bd64b8f012c6a82df
Keven Allan <kevenallan98@gmail.com>
2025-06-03 13:31:30 -0300

    Trocando os inputs de origem e destino, por autocomplete, para facilitar a escolha dos endereços

--- src/app/pages/rota/rota.component.ts
+++ src/app/pages/rota/rota.component.ts
@@ -8,6 +8,7 @@ import { MapaService } from '../../core/service/mapa.service';
 import { environment } from '../../../environments/environment';
 
 import { InputTextModule } from 'primeng/inputtext';
+import { AutoCompleteModule } from 'primeng/autocomplete';
 import { ButtonModule } from 'primeng/button';
 import { FloatLabel } from 'primeng/floatlabel';
 
@@ -22,6 +23,7 @@ import { firstValueFrom } from 'rxjs';
     imports: [
         FormsModule,
         InputTextModule,
+        AutoCompleteModule,
         FloatLabel,
         ButtonModule,
         HttpClientModule,
@@ -31,82 +33,79 @@ export class RotaComponent implements AfterViewInit {
     private map!: L.Map;
     private rotaControl: any;
 
-    private ORS_API_KEY =
-        '5b3ce3597851110001cf6248b6230345a07942ff8963b64258d71297';
+    origem: any;
+    destino: any;
 
-    origem = 'mangabeira shopping';
-    destino = 'almeidão';
+    origemSuggestions: any[] = [];
+    destinoSuggestions: any[] = [];
 
     origemCoord: L.LatLng | null = null;
     destinoCoord: L.LatLng | null = null;
+    tipoSelecionadoParaMarcador: 'origem' | 'destino' | null = null;
 
     private markerOrigem?: L.Marker;
     private markerDestino?: L.Marker;
-
-    private modoSelecionando: 'origem' | 'destino' | null = null;
 
     constructor(private mapaService: MapaService, private http: HttpClient) {}
 
     ngAfterViewInit(): void {
         this.map = this.mapaService.initMap();
-        console.log('L.Routing', L.Routing);
 
         this.map.on('click', (e: L.LeafletMouseEvent) => {
-            if (this.modoSelecionando === 'origem') {
-                this.origemCoord = e.latlng;
+            if (!this.tipoSelecionadoParaMarcador) return;
+
+            const coord = e.latlng;
+            const marker = L.marker(coord).addTo(this.map);
+
+            if (this.tipoSelecionadoParaMarcador === 'origem') {
                 if (this.markerOrigem) this.map.removeLayer(this.markerOrigem);
-                this.markerOrigem = L.marker(e.latlng).addTo(this.map);
-                this.modoSelecionando = null;
-            } else if (this.modoSelecionando === 'destino') {
-                this.destinoCoord = e.latlng;
+                this.markerOrigem = marker;
+                this.origemCoord = coord;
+            } else if (this.tipoSelecionadoParaMarcador === 'destino') {
                 if (this.markerDestino)
                     this.map.removeLayer(this.markerDestino);
-                this.markerDestino = L.marker(e.latlng).addTo(this.map);
-                this.modoSelecionando = null;
+                this.markerDestino = marker;
+                this.destinoCoord = coord;
+            }
+
+            // Resetar após o clique
+            this.tipoSelecionadoParaMarcador = null;
+        });
+    }
+
+    buscarSugestoes(event: any, tipo: 'origem' | 'destino') {
+        const query = event.query;
+        const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(
+            query
+        )}&countrycodes=br&addressdetails=1&limit=5`;
+
+        this.http.get<any[]>(url).subscribe((data) => {
+            if (tipo === 'origem') {
+                this.origemSuggestions = data;
+            } else {
+                this.destinoSuggestions = data;
             }
         });
     }
 
-    async buscarEndereco(tipo: 'origem' | 'destino') {
+    selecionarEndereco(tipo: 'origem' | 'destino') {
         const endereco = tipo === 'origem' ? this.origem : this.destino;
         if (!endereco) return;
 
-        const coord = await this.getCoordinates(endereco);
+        const coord = L.latLng(+endereco.lat, +endereco.lon);
 
-        if (coord) {
-            this.map.setView(coord, 16);
-            this.modoSelecionando = tipo;
-            alert(
-                `Agora clique no mapa para escolher o ponto exato da ${tipo}.`
-            );
-        } else {
-            alert(`Endereço de ${tipo} não encontrado.`);
-        }
+        this.map.setView(coord, 16);
+
+        // Não adiciona marcador aqui, apenas guarda o tipo de marcador que será adicionado no clique
+        this.tipoSelecionadoParaMarcador = tipo;
     }
 
-    async getCoordinates(address: string): Promise<L.LatLng | null> {
-        const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(
-            address
-        )}`;
-        try {
-            const data: any = await firstValueFrom(this.http.get<any[]>(url));
-            if (data.length > 0) {
-                const lat = parseFloat(data[0].lat);
-                const lon = parseFloat(data[0].lon);
-                return L.latLng(lat, lon);
-            }
-            return null;
-        } catch (error) {
-            console.error('Erro ao buscar coordenadas:', error);
-            return null;
-        }
-    }
     routeLayer!: L.GeoJSON;
     userMarker: L.Marker | null = null;
 
     montarRota() {
         if (!this.origemCoord || !this.destinoCoord) {
-            alert('Você precisa clicar no mapa após digitar os endereços.');
+            alert('Você precisa selecionar a origem e o destino.');
             return;
         }
 
@@ -123,17 +122,11 @@ export class RotaComponent implements AfterViewInit {
             )
             .subscribe({
                 next: (res) => {
-                    console.log('Resposta da rota:', res);
-
-                    // Se já existe uma rota, remova antes de adicionar a nova
                     if (this.routeLayer) {
                         this.map.removeLayer(this.routeLayer);
                     }
 
-                    // Cria uma camada GeoJSON com a rota
                     this.routeLayer = L.geoJSON(res).addTo(this.map);
-
-                    // Ajusta o zoom do mapa para caber toda a rota
                     this.map.fitBounds(this.routeLayer.getBounds());
                     this.watchUserLocation();
                 },
@@ -145,6 +138,7 @@ export class RotaComponent implements AfterViewInit {
                 },
             });
     }
+
     private watchUserLocation(): void {
         if (navigator.geolocation) {
             navigator.geolocation.watchPosition(
@@ -152,13 +146,9 @@ export class RotaComponent implements AfterViewInit {
                     const lat = pos.coords.latitude;
                     const lng = pos.coords.longitude;
 
-                    console.log('Movimento detectado:', lat, lng);
-
-                    // Atualiza o mapa para o novo local (opcional)
                     this.map.setView([lat, lng], 16);
 
                     if (!this.userMarker) {
-                        // Cria o marker apenas uma vez
                         this.userMarker = L.marker([lat, lng])
                             .addTo(this.map)
                             .bindPopup(
@@ -166,7 +156,6 @@ export class RotaComponent implements AfterViewInit {
                             )
                             .openPopup();
                     } else {
-                        // Atualiza a posição do marker existente
                         this.userMarker
                             .setLatLng([lat, lng])
                             .getPopup()
